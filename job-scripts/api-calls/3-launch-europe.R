091dbf5c513a7faeca287b855e6358e0b7b17241
Alessandro Balconi <alessandro.balconi@outlook.it>
2021-08-26 15:47:08 +0000

    same "fix" to timeout as americas

--- job-scripts/api-calls/3-launch-europe.R
+++ job-scripts/api-calls/3-launch-europe.R
@@ -8,4 +8,7 @@ tryCatch({
     body = "There was an error during API calls for EUROPE server. Manual restart required."
   )
   print(e)
+}, finally = {
+  rm(list = ls(all.names = TRUE))
+  source("/home/balco/dev/lor-meta-report/job-scripts/api-calls/main-scripts/3-EUROPE-mongo-scraper.R")
 })
