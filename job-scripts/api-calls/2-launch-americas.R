5fc3507cbec14937be1e6835434f02e3321c3efc
Alessandro Balconi <alessandro.balconi@outlook.it>
2021-08-26 07:03:43 +0000

    temporary "fix" to americas' timeouts

--- job-scripts/api-calls/2-launch-americas.R
+++ job-scripts/api-calls/2-launch-americas.R
@@ -8,4 +8,7 @@ tryCatch({
     body = "There was an error during API calls for AMERICAS server. Manual restart required."
   )
   print(e)
+}, finally = {
+  rm(list = ls(all.names = TRUE))
+  source("/home/balco/dev/lor-meta-report/job-scripts/api-calls/main-scripts/2-AMERICAS-mongo-scraper.R")
 })
