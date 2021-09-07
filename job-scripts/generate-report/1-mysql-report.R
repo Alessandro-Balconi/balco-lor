779dceeec8d3208cf985327bde65abe4d8ba0a8b
Alessandro Balconi <alessandro.balconi@outlook.it>
2021-09-07 07:34:55 +0000

    minor formatting changes

--- job-scripts/generate-report/1-mysql-report.R
+++ job-scripts/generate-report/1-mysql-report.R
@@ -826,7 +826,7 @@ tbl <- data %>%
         name = "Deck Code",
         html = TRUE, 
         cell = function(value) {
-          sprintf('<a href="https://lor.runeterra.ar/decks/code/%s" target="_blank">%s</a>', value, value)
+          sprintf('<a href="https://lor.runeterra.ar/decks/code/%s" target="_blank">%s</a>', value, str_trunc(deck_code, width = 18))
         })
     ),
     wrap = FALSE,
