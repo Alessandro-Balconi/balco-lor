f78b10eefae2cc0c61fde63d71239ba1dc89690a
Alessandro Balconi <alessandro.balconi@outlook.it>
2021-10-10 13:43:50 +0000

    mr15 parameters update

--- job-scripts/publish-report/publish_report.R
+++ job-scripts/publish-report/publish_report.R
@@ -12,10 +12,10 @@ template_file_1 <- "/home/balco/dev/lor-meta-report/templates/report_pt1.md" # t
 template_file_2 <- "/home/balco/dev/lor-meta-report/templates/report_pt2.md" # this should not be changed
 
 # this should be changed every week
-p_report_number <- "14"
+p_report_number <- "15"
 p_full_art      <- "03IO006"
-p_emote         <- "ekko"
-p_subtitle      <- "Patch 2.16 - Week 2"
+p_emote         <- "leona"
+p_subtitle      <- "Patch 2.17 - Week 1"
 
 # 3. functions ----
 
