95613a7646c250b25dccca2ad1d391583a7638b0
Alessandro Balconi <alessandro.balconi@outlook.it>
2021-10-10 13:44:08 +0000

    added gs4_report to the list of daily jobs

--- 0-launch.R
+++ 0-launch.R
@@ -3,15 +3,25 @@ tryCatch({
   if(lubridate::hour(Sys.time()) == 0) { cat(sprintf("--- %s --- \n", Sys.Date())) }
   tictoc::tic()
   source("/home/balco/dev/lor-meta-report/job-scripts/mongo-to-sql/4-ASIA-mongo-to-mysql.R")
+  tictoc::toc()
+  tictoc::tic()
   rm(list = ls(all.names = TRUE))
   source("/home/balco/dev/lor-meta-report/job-scripts/mongo-to-sql/3-AMERICAS-mongo-to-mysql.R")
+  tictoc::toc()
+  tictoc::tic()
   rm(list = ls(all.names = TRUE))
   source("/home/balco/dev/lor-meta-report/job-scripts/mongo-to-sql/2-mongo-to-mysql.R")
+  tictoc::toc()
+  tictoc::tic()
   rm(list = ls(all.names = TRUE))
   source("/home/balco/dev/lor-meta-report/job-scripts/mongo-to-sql/5-mysql-matchup-table.R")
+  tictoc::toc()
+  tictoc::tic()
   rm(list = ls(all.names = TRUE))
   source("/home/balco/dev/lor-meta-report/job-scripts/mongo-to-sql/7-mysql-decklists.R")
   tictoc::toc()
+  rm(list = ls(all.names = TRUE))
+  source("/home/balco/dev/lor-meta-report/job-scripts/gs4-report/gs4_report.R")
 }, error = function(e) {
   RPushbullet::pbPost(
     "note",
