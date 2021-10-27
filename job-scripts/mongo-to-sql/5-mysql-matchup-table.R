319218ffe715d5730c6e6529d4228f1144ba8c16
Alessandro Balconi <alessandro.balconi@outlook.it>
2021-10-27 11:45:59 +0000

    added table matchup v2

--- job-scripts/mongo-to-sql/5-mysql-matchup-table.R
+++ job-scripts/mongo-to-sql/5-mysql-matchup-table.R
@@ -63,6 +63,11 @@ min_date <- tbl(con, "lor_match_info_na") %>%
   mutate(min_date = min_date + lubridate::days(1)) %>% 
   pull()
 
+# archetypes aggregation mapping
+archetypes_map <- with_gs4_quiet(read_sheet(ss = "1Xlh2kg7gLzvqugqGPpI4PidAdM5snggbJ44aRLuik5E", sheet = 'Archetypes Mapping'))
+
+# 5.2 table of current patch ----
+
 # extract data from MySQL
 data <- tbl(con, "lor_match_info_na") %>%
   union_all(tbl(con, "lor_match_info")) %>% 
@@ -70,89 +75,14 @@ data <- tbl(con, "lor_match_info_na") %>%
   filter(str_detect(game_version, current_patch)) %>% 
   mutate(game_start_time_utc = sql("CAST(game_start_time_utc AS DATETIME)")) %>% 
   filter(game_start_time_utc >= min_date) %>% 
-  mutate(last_d = case_when(game_start_time_utc >= last3_date ~ 2, game_start_time_utc >= last7_date ~ 1, TRUE ~ 0)) %>% 
-  select(match_id, game_outcome, archetype, last_d) %>% 
+  select(match_id, game_outcome, archetype) %>% 
   collect()
 
 # merge archetypes according to mapping
-archetypes_map <- with_gs4_quiet(read_sheet(ss = "1Xlh2kg7gLzvqugqGPpI4PidAdM5snggbJ44aRLuik5E", sheet = 'Archetypes Mapping'))
-
 data <- data %>%
   left_join(archetypes_map, by = c("archetype" = "old_name")) %>%
   mutate(archetype = ifelse(!is.na(new_name), new_name, archetype)) %>%
   select(-new_name)
-
-# 5.1 table of last 3 days ----
-
-# keep only data for last 3 days
-data_3d <- data %>% 
-  filter(last_d > 1) %>% 
-  select(-last_d)
-
-# calculate matchup information
-data_matchup_3d <- data_3d %>%
-  group_by(match_id) %>%
-  arrange(match_id, archetype) %>% 
-  mutate(id = row_number()) %>% 
-  mutate(winner = case_when(id == 1 & game_outcome == "win" ~ 1, id == 2 & game_outcome == "win" ~ 2, TRUE ~ 0)) %>% 
-  pivot_wider(names_from = id, values_from = archetype, names_prefix = "archetype_") %>% 
-  fill(starts_with("archetype_"), .direction = "updown") %>% 
-  ungroup() %>% 
-  filter(winner != 0) %>%
-  group_by(across(starts_with("archetype_"))) %>% 
-  summarise(n = n(), wins = sum(winner == 1), .groups = "drop") %>% 
-  mutate(winrate = ifelse(archetype_1 == archetype_2, 0.5, wins / n)) 
-
-# also calculate the matchup information from the opponent's POV
-data_matchup2_3d <- tibble(
-  archetype_1 = data_matchup_3d$archetype_2,
-  archetype_2 = data_matchup_3d$archetype_1,
-  n = data_matchup_3d$n,
-  winrate = 1- data_matchup_3d$winrate
-)
-
-data_matchup_3d <- data_matchup_3d %>% 
-  select(-wins) %>% 
-  bind_rows(data_matchup2_3d)
-
-# 5.1 table of last 7 days ----
-
-# keep only data for last 7 days
-data_7d <- data %>% 
-  filter(last_d > 0) %>% 
-  select(-last_d)
-
-# calculate matchup information
-data_matchup_7d <- data_7d %>%
-  group_by(match_id) %>%
-  arrange(match_id, archetype) %>% 
-  mutate(id = row_number()) %>% 
-  mutate(winner = case_when(id == 1 & game_outcome == "win" ~ 1, id == 2 & game_outcome == "win" ~ 2, TRUE ~ 0)) %>% 
-  pivot_wider(names_from = id, values_from = archetype, names_prefix = "archetype_") %>% 
-  fill(starts_with("archetype_"), .direction = "updown") %>% 
-  ungroup() %>% 
-  filter(winner != 0) %>%
-  group_by(across(starts_with("archetype_"))) %>% 
-  summarise(n = n(), wins = sum(winner == 1), .groups = "drop") %>% 
-  mutate(winrate = ifelse(archetype_1 == archetype_2, 0.5, wins / n)) 
-
-# also calculate the matchup information from the opponent's POV
-data_matchup2_7d <- tibble(
-  archetype_1 = data_matchup_7d$archetype_2,
-  archetype_2 = data_matchup_7d$archetype_1,
-  n = data_matchup_7d$n,
-  winrate = 1- data_matchup_7d$winrate
-)
-
-data_matchup_7d <- data_matchup_7d %>% 
-  select(-wins) %>% 
-  bind_rows(data_matchup2_7d)
-
-# 5.2 table of current patch ----
-
-# remove last 7 days information
-data <- data %>% 
-  select(-last_d)
 
 # calculate matchup information
 data_matchup <- data %>%
@@ -180,6 +110,49 @@ data_matchup <- data_matchup %>%
   select(-wins) %>% 
   bind_rows(data_matchup2)
 
+# 5.3 table v2 ----
+
+# extract data from MySQL
+data_v2 <- tbl(con, "lor_match_info_na") %>%
+  union_all(tbl(con, "lor_match_info")) %>% 
+  union_all(tbl(con, "lor_match_info_asia")) %>% 
+  filter(str_detect(game_version, current_patch)) %>% 
+  mutate(game_start_time_utc = sql("CAST(game_start_time_utc AS DATETIME)")) %>% 
+  filter(game_start_time_utc >= min_date) %>% 
+  mutate(time_frame = case_when(game_start_time_utc >= last3_date ~ 2, game_start_time_utc >= last7_date ~ 1, TRUE ~ 0)) %>% 
+  select(match_id, game_outcome, archetype, time_frame, is_master) %>% 
+  collect()
+
+data_matchup_v2 <- data_v2 %>%
+  left_join(archetypes_map, by = c("archetype" = "old_name")) %>%
+  mutate(archetype = ifelse(!is.na(new_name), new_name, archetype)) %>%
+  select(-new_name)
+
+data_matchup_v2 <- data_matchup_v2 %>% 
+  arrange(match_id, archetype) %>% 
+  group_by(match_id) %>% 
+  mutate(id = row_number()) %>% 
+  ungroup()
+
+opponent_data <- data_matchup_v2 %>% 
+  mutate(id = case_when(id == 1 ~ 2, id == 2 ~ 1, TRUE ~ 0)) %>% 
+  select(match_id, id, archetype_2 = archetype)
+
+data_matchup_v2 <- data_matchup_v2 %>% 
+  left_join(opponent_data, by = c("match_id", "id")) %>% 
+  select(-id) %>% 
+  rename(archetype_1 = archetype)
+
+n_match <- data_matchup_v2 %>% 
+  count(archetype_1, archetype_2, time_frame, is_master)
+
+data_matchup_v2 <- data_matchup_v2 %>%
+  group_by(archetype_1, archetype_2, time_frame, is_master) %>% 
+  summarise(wins = sum(game_outcome == "win"), .groups = "drop") %>% 
+  left_join(n_match, by = c("archetype_1", "archetype_2", "time_frame", "is_master")) %>% 
+  mutate(winrate = ifelse(archetype_1 == archetype_2, 0.5, wins / n)) %>% 
+  select(-wins)
+
 # 6. save to MySQL db ----
 
 if(nrow(data_matchup) >  0){
@@ -187,16 +160,17 @@ if(nrow(data_matchup) >  0){
   data_matchup %>% 
     DBI::dbWriteTable(conn = con, name = "lor_matchup_table", value = ., overwrite = TRUE, row.names = FALSE) 
 
-  data_matchup_7d %>% 
-    DBI::dbWriteTable(conn = con, name = "lor_matchup_table_7d", value = ., overwrite = TRUE, row.names = FALSE) 
-  
-  data_matchup_3d %>% 
-    DBI::dbWriteTable(conn = con, name = "lor_matchup_table_3d", value = ., overwrite = TRUE, row.names = FALSE) 
-  
   Sys.time() %>%
     as_tibble() %>% 
     DBI::dbWriteTable(conn = con, name = "lor_update_time", value = ., overwrite = TRUE, row.names = FALSE) 
   
+}
+
+if(nrow(data_matchup_v2) >  0){
+  
+  data_matchup_v2 %>% 
+    DBI::dbWriteTable(conn = con, name = "lor_matchup_table_v2", value = ., overwrite = TRUE, row.names = FALSE) 
+
 }
 
 DBI::dbDisconnect(con)
