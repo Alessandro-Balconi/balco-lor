8e2ecb0f7fe2c58a1240337d73d3f9221aa42154
Alessandro Balconi <alessandro.balconi@outlook.it>
2022-01-01 15:13:36 +0000

    image with matches played by day of the season

---
+++ other-scripts/matches_played_by_season_day.R
@@ -0,0 +1,53 @@
+x = tbl(con, 'lor_match_info_v2') %>% 
+  mutate(ts = sql('CAST(game_start_time_utc AS DATE)')) %>% 
+  count(ts) %>%
+  arrange(ts) %>% 
+  collect()
+
+y = tbl(con, 'lor_match_info_old_v2') %>% 
+  mutate(ts = sql('CAST(game_start_time_utc AS DATE)')) %>% 
+  filter(ts >= local(ymd('2021-10-20')) & ts < local(ymd('2021-12-09'))) %>%
+  count(ts) %>%
+  arrange(ts) %>% 
+  collect()
+
+w = tbl(con, 'lor_match_info_old_v2') %>% 
+  mutate(ts = sql('CAST(game_start_time_utc AS DATE)')) %>% 
+  filter(ts >= local(ymd('2021-08-27')) & ts < local(ymd('2021-10-20'))) %>%
+  count(ts) %>%
+  arrange(ts) %>% 
+  collect()
+
+x = x %>% 
+  mutate(n = as.numeric(n)) %>% 
+  filter(ts >= Sys.Date() - lubridate::days(30) & ts < Sys.Date()) %>% 
+  filter(ts >= ymd('2021-12-09')) %>% 
+  mutate(season = 'Magic Misadventures', day = row_number())
+  
+y = y %>% 
+  mutate(n = as.numeric(n)) %>% 
+  filter(ts >= ymd('2021-10-21')) %>% 
+  mutate(season = 'Between Worlds', day = row_number())
+
+w = w %>% 
+  mutate(n = as.numeric(n)) %>% 
+  filter(ts >= ymd('2021-08-27')) %>% 
+  mutate(season = 'Beyond the Bandlewood', day = row_number())
+
+z = bind_rows(w, x,y)
+
+ggplot(z, aes(x = day, y = n, color = season)) +
+  geom_line(size = 1.5) +
+  geom_point(alpha = 0.3, size = 3) +
+  expand_limits(y = 0) +
+  theme_bw() +
+  scale_y_continuous(labels = scales::comma_format()) +
+  theme(legend.position = 'bottom') +
+  labs(
+    x = 'Day of the Season', 
+    y = '# of Matches', 
+    title = 'Matches collected by day of the season', 
+    color = 'Season',
+    subtitle = 'Plat+ data'
+  )
+  
