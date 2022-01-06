895d966d7b7dbc91aaedaee5fde6d532c53b7f13
Alessandro Balconi <alessandro.balconi@outlook.it>
2022-01-06 11:04:15 +0000

    delete non ranked matches older than 1 month from dbs

--- job-scripts/weekly-db-cleanup/5-weekly-mongodb-cleanup.R
+++ job-scripts/weekly-db-cleanup/5-weekly-mongodb-cleanup.R
@@ -1,6 +1,6 @@
 # Weekly database cleanup
 
-# Removes matches older than 1 month from the standard MongoDB and stores them in the "db_old" version of those databases
+# Removes matches older than 1 month from the standard MongoD
 # Runs weekly on Wednesday Morning (why? idk)
 
 tictoc::tic()
@@ -20,40 +20,10 @@ m_db1 <- mongo(url = sprintf("mongodb://%s:%s@localhost:27017/admin", db_creds$u
 m_db2 <- mongo(url = sprintf("mongodb://%s:%s@localhost:27017/admin", db_creds$uid, db_creds$pwd), collection = "lor_match_info_na")
 m_db3 <- mongo(url = sprintf("mongodb://%s:%s@localhost:27017/admin", db_creds$uid, db_creds$pwd), collection = "lor_match_info_asia")
 
-# "old" dbs
-m_db1_old <- mongo(url = sprintf("mongodb://%s:%s@localhost:27017/admin", db_creds$uid, db_creds$pwd), collection = "lor_match_info_old")
-m_db2_old <- mongo(url = sprintf("mongodb://%s:%s@localhost:27017/admin", db_creds$uid, db_creds$pwd), collection = "lor_match_info_na_old")
-m_db3_old <- mongo(url = sprintf("mongodb://%s:%s@localhost:27017/admin", db_creds$uid, db_creds$pwd), collection = "lor_match_info_asia_old")
-
 # 3. extract old matches from dbs & add them to "old_db" & remove them from current dbs----
 
-old_eu   <- m_db1$find(sprintf('{"info.game_start_time_utc": { "$lt" : { "$date" : "%sT00:00:00Z" }}}', Sys.Date() - lubridate::days(30)))
-
-if(nrow(old_eu) > 0){
-  
-  m_db1_old$insert(old_eu)
-  m_db1$remove(sprintf('{"info.game_start_time_utc": { "$lt" : { "$date" : "%sT00:00:00Z" }}}', Sys.Date() - lubridate::days(30)))
-  
-}
-
-rm(old_eu)
-old_na   <- m_db2$find(sprintf('{"info.game_start_time_utc": { "$lt" : { "$date" : "%sT00:00:00Z" }}}', Sys.Date() - lubridate::days(30)))
-
-if(nrow(old_na) > 0){
-  
-  m_db2_old$insert(old_na)
-  m_db2$remove(sprintf('{"info.game_start_time_utc": { "$lt" : { "$date" : "%sT00:00:00Z" }}}', Sys.Date() - lubridate::days(30)))
-  
-}
-
-rm(old_na)
-old_asia <- m_db3$find(sprintf('{"info.game_start_time_utc": { "$lt" : { "$date" : "%sT00:00:00Z" }}}', Sys.Date() - lubridate::days(30)))
-
-if(nrow(old_asia) > 0){
-  
-  m_db3_old$insert(old_asia)
-  m_db3$remove(sprintf('{"info.game_start_time_utc": { "$lt" : { "$date" : "%sT00:00:00Z" }}}', Sys.Date() - lubridate::days(30)))
-  
-}
+m_db1$remove(sprintf('{"info.game_start_time_utc": { "$lt" : { "$date" : "%sT00:00:00Z" }}}', Sys.Date() - lubridate::days(30)))
+m_db2$remove(sprintf('{"info.game_start_time_utc": { "$lt" : { "$date" : "%sT00:00:00Z" }}}', Sys.Date() - lubridate::days(30)))
+m_db3$remove(sprintf('{"info.game_start_time_utc": { "$lt" : { "$date" : "%sT00:00:00Z" }}}', Sys.Date() - lubridate::days(30)))
 
 tictoc::toc()
