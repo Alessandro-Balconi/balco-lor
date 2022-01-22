a7d31b76fe14f19578ae23af8a4ce8ba09b26f58
Alessandro Balconi <alessandro.balconi@outlook.it>
2022-01-22 10:28:24 +0000

    created new 'ranked_archetypes' model

---
+++ job-scripts/data-models/2-ranked_archetypes.R
@@ -0,0 +1,104 @@
+# Creates a MySQL table with archetype informations for the current patch (from data of MySQL databases)
+
+# This task is performed with daily frequency at 16.30 UTC
+
+# 1. libraries ----
+
+suppressPackageStartupMessages(library(tidyverse)) # all purposes package
+suppressPackageStartupMessages(library(googlesheets4)) # working with google spreadsheets
+
+# 2. parameters ----
+
+options(gargle_oauth_email = "Balco21@outlook.it")
+options(googlesheets4_quiet = TRUE)
+
+# load mysql db credentials
+db_creds <- config::get("mysql", file = "/home/balco/my_rconfig.yml")
+
+# date of 3/7 days ago
+last7_date <- (Sys.time() - lubridate::days(7))
+last3_date <- (Sys.time() - lubridate::days(3))
+
+# 3. functions ----
+
+# 4. connect to db & load data ----
+
+# close previous connections to MySQL database (if any)
+if(exists("con")){ DBI::dbDisconnect(con) }
+
+# create connection to MySQL database
+con <- DBI::dbConnect(
+  RMariaDB::MariaDB(),
+  db_host = "127.0.0.1",
+  user = db_creds$uid,
+  password = db_creds$pwd,
+  dbname = db_creds$dbs
+)
+
+# 5. prepare table ----
+
+patches_to_analyze <- tbl(con, "lor_patch_history") %>% 
+  collect() %>% 
+  arrange(-last_patch) %>% 
+  mutate(new_change = lag(change)) %>% 
+  replace_na(list(new_change = 0)) %>% 
+  mutate(cum_change = cumsum(new_change)) %>% 
+  filter(cum_change == min(cum_change))
+
+# fix for single digits patches
+patches_to_analyze <- patches_to_analyze %>% 
+  mutate(value = str_replace_all(value, pattern = "\\.", replacement = "_")) %>% 
+  separate(col = value, into = c('main', 'sub'), sep ="_") %>% 
+  mutate(sub = str_pad(sub, width = 2, pad = "0", side = 'left')) %>% 
+  unite(col = value, main, sub, sep = "_") %>% 
+  pull(value)
+
+current_patch <- patches_to_analyze %>% 
+  paste0("live_", ., "_") %>% 
+  paste0(collapse = "|")
+
+# start collecting matches only 24 hours after the patch
+min_date <- tbl(con, "lor_match_info_v2") %>% 
+  filter(str_detect(game_version, current_patch)) %>%
+  select(game_start_time_utc) %>% 
+  mutate(game_start_time_utc = sql("CAST(game_start_time_utc AS DATETIME)")) %>% 
+  summarise(min_date = min(game_start_time_utc, na.rm = TRUE)) %>% 
+  collect() %>% 
+  mutate(min_date = min_date + lubridate::days(1)) %>% 
+  pull()
+#min_date <- as.POSIXct("2021-12-14 18:00:00 UTC") # hotfix date
+
+# archetypes aggregation mapping
+archetypes_map <- with_gs4_quiet(read_sheet(ss = "1Xlh2kg7gLzvqugqGPpI4PidAdM5snggbJ44aRLuik5E", sheet = 'Archetypes Mapping'))
+
+# 5.3 table v2 ----
+
+# extract data from MySQL
+data_v2 <- tbl(con, "lor_match_info_v2") %>%
+  filter(str_detect(game_version, current_patch)) %>% 
+  mutate(game_start_time_utc = sql("CAST(game_start_time_utc AS DATETIME)")) %>% 
+  filter(game_start_time_utc >= min_date) %>% 
+  mutate(time_frame = case_when(game_start_time_utc >= last3_date ~ 2, game_start_time_utc >= last7_date ~ 1, TRUE ~ 0)) %>% 
+  count(region, archetype, game_outcome, time_frame, is_master) %>% 
+  collect()
+
+data_v2 <- data_v2 %>%
+  ungroup() %>% 
+  left_join(archetypes_map, by = c("archetype" = "old_name")) %>%
+  mutate(archetype = ifelse(!is.na(new_name), new_name, archetype)) %>%
+  select(-new_name)
+
+data_v2 = data_v2 %>% 
+  mutate(n = as.numeric(n)) %>% 
+  pivot_wider(names_from = game_outcome, values_from = n, values_fill = 0, values_fn = sum)
+
+# 6. save to MySQL db ----
+
+if(nrow(data_v2) >  0){
+  
+  data_v2 %>% 
+    DBI::dbWriteTable(conn = con, name = "ranked_archetypes", value = ., overwrite = TRUE, row.names = FALSE) 
+  
+}
+
+DBI::dbDisconnect(con)
