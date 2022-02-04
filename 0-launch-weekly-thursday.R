acd6bcf82b3048bfad4e27238e83b43f5a3dec92
Alessandro Balconi <alessandro.balconi@outlook.it>
2022-02-04 08:45:02 +0000

    file rename

--- 0-launch-weekly-thursday.R
+++ 0-launch-weekly-thursday.R
@@ -2,7 +2,7 @@
 # This script runs every Thursday (in the morning, around 10.00 CEST)
 tryCatch({
   cat(sprintf("--- %s --- \n", Sys.Date()))
-  source("/home/balco/dev/lor-meta-report/job-scripts/mongo-to-sql/8-mysql-patch-history.R")
+  source("/home/balco/dev/lor-meta-report/job-scripts/data-models/4-utils_patch_history.R")
 }, error = function(e) {
   RPushbullet::pbPost(
     "note",
