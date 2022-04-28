fca1d4d675c065e67f89f9a836cd2186ae50d64d
Alessandro Balconi <alessandro.balconi@outlook.it>
2022-04-28 13:20:40 +0000

    graphic improvements to plot

--- other-scripts/nice_patch_changes_effect.R
+++ other-scripts/nice_patch_changes_effect.R
@@ -40,6 +40,11 @@ x = tbl(con, 'ranked_daily_archetypes') %>%
   summarise(across(c(win, match), sum, na.rm = TRUE), .groups = 'drop') %>% 
   collect()
 
+ndecks = x %>% 
+  filter(patch == '3.6') %>% 
+  summarise(n = sum(match)) %>% 
+  pull(n)
+
 xx = x %>% 
   group_by(patch) %>% 
   summarise(tot_match = sum(match), .groups = 'drop') %>% 
@@ -47,37 +52,37 @@ xx = x %>%
   mutate(pr = match / tot_match, wr = win / match) %>% 
   select(archetype, patch, pr, wr) %>% 
   pivot_wider(names_from = patch, values_from = c(pr, wr)) %>% 
-  arrange(-pr_3.5)
+  arrange(-pr_3.6) %>% 
+  head(15) %>% 
+  mutate(pr_change = pr_3.6 - pr_3.5, wr_change = wr_3.6 - wr_3.5)
 
 xx = xx %>% 
   mutate(archetype = factor(archetype, levels = unique(xx$archetype), ordered = TRUE))
 
-ndecks = x %>% 
-  filter(patch == '3.6') %>% 
-  summarise(n = sum(match)) %>% 
-  pull(n)
-
-xxx = xx %>% 
-  head(10) %>% 
-  mutate(pr_change = pr_3.6 - pr_3.5, wr_change = wr_3.6 - wr_3.5)
-
-xxx %>% 
+xx %>% 
   pivot_longer(cols = -c(archetype)) %>%
-  #pivot_longer(cols = -c(archetype, matches('_change'))) %>%
-  mutate(name = factor(name, levels = c('pr_3.5', 'wr_3.5', 'pr_3.6', 'wr_3.6', 'pr_change', 'wr_change'), ordered = TRUE)) %>%
-  mutate(fill_tile = if_else(str_detect(name, 'wr_'), 0 , 1)) %>% 
+  mutate(name = factor(name, levels = c('pr_3.6', 'wr_3.6', 'pr_3.5', 'wr_3.5', 'pr_change', 'wr_change'), ordered = TRUE)) %>%
+  mutate(what_tile = if_else(str_detect(name, 'change'), 'change' , if_else(str_detect(name, 'wr'), 'wr', 'pr'))) %>% 
   ggplot(aes(x = name, y = reorder(archetype, desc(archetype)))) +
-  geom_tile(aes(alpha = if_else(fill_tile == 1, value, 0)), fill = 'steelblue') +
+  geom_tile(aes(alpha = if_else(what_tile == 'pr', value, if_else(what_tile == 'wr', (value-0.4)/2, 0)), fill = what_tile)) +
   shadowtext::geom_shadowtext(
     aes(
       label = if_else(str_detect(name, 'change', negate = TRUE), scales::percent(value, accuracy = .1), scales_percent_plus(value, accuracy = .1)),
-      color = if_else(str_detect(name, 'change', negate = TRUE), "white", if_else(value > 0, 'forestgreen', 'red'))
+      color = case_when(
+        str_detect(name, 'change', negate = TRUE) ~ "not_colored", 
+        value > 0.05 ~ 'super_pos', 
+        value > 0.005 ~ 'pos', 
+        value < -0.05 ~ 'super_neg', 
+        value < -0.005 ~ 'neg', 
+        TRUE ~ 'not_changed'
+      )
     ), 
-    size = 12, na.rm = TRUE) +
-  scale_y_discrete(labels = function(x) str_wrap(x, width = 10)) +
-  scale_x_discrete(labels = c('Playrate (3.5)', 'Winrate (3.5)', 'Playrate (3.6)', 'Winrate (3.6)', 'ΔPlayrate', 'ΔWinrate'), position = "top") +
-  scale_color_manual(values = c('white' = 'white', 'forestgreen' = 'forestgreen', 'red' = 'red')) +
-  labs(title = 'Early data from Patch 3.6', subtitle = sprintf('Plat+ - %s decks analyzed', ndecks), x = element_blank(), y = 'Archetype') +
+    bg.r = 0.08, size = 12, na.rm = TRUE) +
+  scale_y_discrete(labels = function(x) str_wrap(x, width = 20)) +
+  scale_x_discrete(labels = c('Playrate (3.6)', 'Winrate (3.6)', 'Playrate (3.5)', 'Winrate (3.5)', 'ΔPlayrate', 'ΔWinrate'), position = "top") +
+  scale_fill_manual(values = c('pr' = 'steelblue', 'wr' = 'forestgreen', 'change' = 'white')) +
+  scale_color_manual(values = c('not_colored' = 'white', 'super_neg' = '#B81D13', 'super_pos' = "#046507", 'pos' = '#149414', 'neg' = 'coral2', 'not_changed' = '#EFB700')) +
+  labs(title = 'Early data from Patch 3.6', subtitle = sprintf('Plat+ - %s decks analyzed', scales::comma(ndecks, accuracy = 1)), x = element_blank(), y = 'Archetype') +
   theme_classic(base_size = 15) +
   theme(legend.position = 'none', axis.text.x = element_text(size = 20))
 
