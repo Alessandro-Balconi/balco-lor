fa2fa2c38fe5d7956b57cda0e7292dbb5abbfe2a
Alessandro Balconi <alessandro.balconi@outlook.it>
2022-04-30 19:31:13 +0000

    add different version of the plot

--- other-scripts/nice_patch_changes_effect.R
+++ other-scripts/nice_patch_changes_effect.R
@@ -86,4 +86,32 @@ xx %>%
   theme_classic(base_size = 20) +
   theme(legend.position = 'none', axis.text.x = element_text(size = 20), axis.text.y = element_text(size = 20))
 
+xx %>% 
+  pivot_longer(cols = -c(archetype)) %>%
+  filter(!name %in% c('pr_3.5', 'wr_3.5')) %>% 
+  mutate(name = factor(name, levels = c('pr_3.6', 'wr_3.6', 'pr_change', 'wr_change'), ordered = TRUE)) %>%
+  mutate(what_tile = if_else(str_detect(name, 'change'), 'change' , if_else(str_detect(name, 'wr'), 'wr', 'pr'))) %>% 
+  ggplot(aes(x = name, y = reorder(archetype, desc(archetype)))) +
+  geom_tile(aes(alpha = if_else(what_tile == 'pr', value, if_else(what_tile == 'wr', (value-0.4)/2, 0)), fill = what_tile)) +
+  shadowtext::geom_shadowtext(
+    aes(
+      label = if_else(str_detect(name, 'change', negate = TRUE), scales::percent(value, accuracy = .1), scales_percent_plus(value, accuracy = .1)),
+      color = case_when(
+        str_detect(name, 'change', negate = TRUE) ~ "not_colored", 
+        value > 0.05 ~ 'super_pos', 
+        value > 0.005 ~ 'pos', 
+        value < -0.05 ~ 'super_neg', 
+        value < -0.005 ~ 'neg', 
+        TRUE ~ 'not_changed'
+      )
+    ), 
+    bg.r = 0.08, size = 12, na.rm = TRUE, bg.color = 'white') +
+  scale_y_discrete(labels = function(x) str_wrap(x, width = 30)) +
+  scale_x_discrete(labels = c('Playrate (3.6)', 'Winrate (3.6)', 'ΔPlayrate vs 3.5', 'ΔWinrate vs 3.5'), position = "top") +
+  scale_fill_manual(values = c('pr' = 'steelblue', 'wr' = 'forestgreen', 'change' = 'white')) +
+  scale_color_manual(values = c('not_colored' = 'black', 'super_neg' = '#B81D13', 'super_pos' = "#046507", 'pos' = '#149414', 'neg' = 'coral2', 'not_changed' = '#EFB700')) +
+  labs(title = 'Patch 3.6 - Most Played Decks', subtitle = sprintf('Plat+ - %s decks analyzed', scales::comma(ndecks, accuracy = 1)), x = element_blank(), y = element_blank()) +
+  theme_classic(base_size = 20) +
+  theme(legend.position = 'none', axis.text.x = element_text(size = 20), axis.text.y = element_text(size = 20))
+
 DBI::dbDisconnect(con)
