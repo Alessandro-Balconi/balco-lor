63f0e1b7c56808ab6ab6955a608961254004d722
Alessandro Balconi <alessandro.balconi@outlook.it>
2022-05-18 10:00:30 +0000

    minor changes

--- other-scripts/new_cards_image.R
+++ other-scripts/new_cards_image.R
@@ -1,40 +1,37 @@
 library(tidyverse)
 
 # list of images to plot
-images <- c(
-  "06BW006T3",
-  "06BW009",
-  "06BW007",
-  "06BW016",
-  "06BW035",
-  "06BW015"#,
-  #"06BW015T5"
-)
+images <- c("06PZ043","06SI037","06BW043","06BC040","06IO036","06DE042","06MT043","06NX037","06SH042")
+
+# to test format before cards come out
+#images <- rep('06IO004', length(images))
 
 # number of the current set
 set_number <- 6
 
 # number of columns, rows in the plot
 n_rows <- 2
-n_cols <- 3
+n_cols <- 5
 
 # convert to df
 data <- tibble(code = images) %>% 
   mutate(img = sprintf('https://dd.b.pvp.net/latest/set%s/en_us/img/cards/%s.png', set_number, code))
 
-# add coordinates
+# add cohordinates
 data <- data %>% 
   mutate(
     x = (rep(seq(from = 1, to = n_cols, by = 1), ceiling(nrow(data) / n_cols)))[1:length(images)],
-    y = (rep(seq(from = 1, to = n_rows, by = 1), ceiling(nrow(data) / n_rows)))[1:length(images)]
+    y = (rep(c(n_rows:1), rep(ceiling(nrow(data) / n_rows), n_rows)))[1:length(images)]
   )
 
+# make plot
 plot <- ggplot(data) +
-  geom_rect(aes(xmin = -Inf, ymin = -Inf, xmax = Inf, ymax = Inf), fill = '#F5F5F5', color = 'white', alpha = 0.1) +
-  ggimage::geom_image(aes(image = img, x = x, y = y), size = 0.2, asp = (1024/680)) +
+  geom_rect(aes(xmin = -Inf, ymin = -Inf, xmax = Inf, ymax = Inf), fill = 'mediumslateblue', alpha = 0.05) +
+  ggimage::geom_image(aes(image = img, x = x, y = y), size = 1/(n_cols+1), asp = (1024/680)) +
   theme_void() +
   expand_limits(y = c(0.5, n_rows+0.5), x = c(0.5, n_cols+0.5))
+  
+# add background
+plot <- ggimage::ggbackground(plot, background = "https://i.imgur.com/t71V6K2.jpeg")
 
-plot <- ggimage::ggbackground(plot, background = "https://wallpaperaccess.com/full/3823169.jpg")
-
-ggsave(filename = "/home/balco/dev/lor-meta-report/data_dumps/new_cards.png", plot = plot, width = 12, height = 8, dpi = 210)
+ggsave(filename = "/home/balco/dev/lor-meta-report/data_dumps/new_cards.png", plot = plot, width = 3, height = 2, dpi = 840)
