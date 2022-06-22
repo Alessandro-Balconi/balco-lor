e2f56ca592501c7e0088560ad573394817fe6611
Alessandro Balconi <alessandro.balconi@outlook.it>
2022-06-22 08:41:20 +0000

    minor changes

--- 0-launch.R
+++ 0-launch.R
@@ -10,8 +10,8 @@ tryCatch({
   cat("MongoDB to MySQL: "); tictoc::toc()
   
   # update data models
-  tictoc::tic()
   rm(list = ls(all.names = TRUE))
+  tictoc::tic()
   source("/home/balco/dev/lor-meta-report/job-scripts/data-models/6-utils_archetype_aggregation.R")
   rm(list = ls(all.names = TRUE))
   source("/home/balco/dev/lor-meta-report/job-scripts/data-models/5-ranked_daily_archetypes.R")
@@ -26,6 +26,7 @@ tryCatch({
   cat("Data Models: "); tictoc::toc()
   
   # update google spreadsheets
+  rm(list = ls(all.names = TRUE))
   tictoc::tic()
   
   # list of files to update
@@ -43,10 +44,13 @@ tryCatch({
   cat("Google Spreadsheets: "); tictoc::toc()
   
 }, error = function(e) {
-  RPushbullet::pbPost(
-    "note",
-    title = "Daily database update",
-    body = "There was an error during the database update. Manual intervention required."
-  )
+  discordr::create_discord_connection(
+    webhook = 'https://discord.com/api/webhooks/940930457070096444/qBSYJH0KETu992oDrdJBH20H1j4yPbBMZm2T3KNKZA5AU1LhRypZshQ0uKly9N_7jeGy',
+    username = 'Daily database update'
+  ) %>% 
+    discordr::send_webhook_message(
+      message = sprintf("Manual intervention required. (%s)", e$message), 
+      conn = .
+    )
   print(e)
 })
