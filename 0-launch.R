aa15eb47dd9f84628c573ea99389cb6e061bd478
Alessandro Balconi <alessandro.balconi@outlook.it>
2022-07-12 11:27:10 +0000

    use my own Google API key to update spreadsheets

--- 0-launch.R
+++ 0-launch.R
@@ -29,6 +29,15 @@ tryCatch({
   rm(list = ls(all.names = TRUE))
   tictoc::tic()
   
+  #options(gargle_oauth_email = "Balco21@outlook.it")
+  options(googlesheets4_quiet = TRUE)
+  
+  # set google API Key & Oauth credentials
+  google_creds <- config::get("google", file = "/home/balco/my_rconfig.yml")
+  gargle::oauth_app_from_json(google_creds$client_secret)
+  googlesheets4::gs4_auth_configure(api_key = google_creds$api_key)
+  googlesheets4::gs4_auth(path = google_creds$auth_path)
+  
   # list of files to update
   gs_list <- list.files('/home/balco/dev/lor-meta-report/job-scripts/daily-google-spreadsheets')
   
