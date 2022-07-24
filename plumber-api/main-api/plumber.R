ee8242e987d75a50cf578ad7f7b9ee4bee3c96a2
Alessandro Balconi <alessandro.balconi@outlook.it>
2022-07-24 08:46:43 +0000

    bugfix: moved db connection opening/closing inside function

--- plumber-api/main-api/plumber.R
+++ plumber-api/main-api/plumber.R
@@ -1,25 +1,27 @@
 library(dplyr)
 
-# load mysql db credentials
-db_creds <- config::get("mysql", file = "/home/balco/my_rconfig.yml")
-
-# create connection to MySQL database
-con <- pool::dbPool(
-  RMariaDB::MariaDB(),
-  db_host  = "127.0.0.1",
-  user     = db_creds$uid,
-  password = db_creds$pwd,
-  dbname   = db_creds$dbs,
-  minSize = 0,
-  idleTimeout = 0
-)
-
 #* LoR-Meta Tier List
 #* @get /player/<region>/<name>
 function(region = '', name = '') {
-
-    tbl(con, 'utils_players') %>% 
-      filter(region == local(region), gameName == local(URLdecode(name))) %>% 
-      pull(tagLine)
+  
+  # load mysql db credentials
+  db_creds <- config::get("mysql", file = "/home/balco/my_rconfig.yml")
+  
+  # create connection to MySQL database
+  con <- DBI::dbConnect(
+    RMariaDB::MariaDB(),
+    db_host  = "127.0.0.1",
+    user     = db_creds$uid,
+    password = db_creds$pwd,
+    dbname   = db_creds$dbs
+  )
+  
+  tag <- tbl(con, 'utils_players') %>% 
+    filter(region == local(region), gameName == local(URLdecode(name))) %>% 
+    pull(tagLine)
+  
+  DBI::dbDisconnect(con)
+    
+  return(tag)
 
 }
