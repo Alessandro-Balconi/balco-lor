8faafefd82e02820b77740a651322501bcd67584
Alessandro Balconi <alessandro.balconi@outlook.it>
2022-08-16 15:43:42 +0000

    code linting

--- 0-launch-weekly-thursday.R
+++ 0-launch-weekly-thursday.R
@@ -1,43 +1,52 @@
 # Runs analysis + shows error if anything happens
 # This script runs every Thursday (in the morning, around 10.00 CEST)
+
+cat(sprintf("--- %s --- \n", Sys.Date()))
+
 tryCatch({
-  cat(sprintf("--- %s --- \n", Sys.Date()))
+  
   source("/home/balco/dev/lor-meta-report/job-scripts/data-models/4-utils_patch_history.R")
+  
 }, error = function(e) {
-  discordr::create_discord_connection(
-    webhook = 'https://discord.com/api/webhooks/940930457070096444/qBSYJH0KETu992oDrdJBH20H1j4yPbBMZm2T3KNKZA5AU1LhRypZshQ0uKly9N_7jeGy',
-    username = "Weekly Patch History Update"
-  ) %>% 
-    discordr::send_webhook_message(
-      message = sprintf("Manual intervention required. (%s)", e$message), 
-      conn = .
-    )
+  
+  lorr::send_discord_message(
+    username = 'Weekly Patch History Update',
+    message = sprintf("Manual intervention required. (%s)", e$message)
+  )
+  
   print(e)
+  
 })
 
 rm(list = ls(all.names = TRUE))
+
 tryCatch({
+  
   tictoc::tic()
+  
   #source("/home/balco/dev/lor-meta-report/job-scripts/mongo-to-sql/9-expeditions-mongo-to-sql.R")
   #rm(list = ls(all.names = TRUE))
   source("/home/balco/dev/lor-meta-report/job-scripts/weekly-db-cleanup/expedition-mysql-cleanup.R")
   #rm(list = ls(all.names = TRUE))
   #source("/home/balco/dev/lor-meta-report/job-scripts/data-models/1-expedition_cards.R")
+  
   tictoc::toc()
+  
 }, error = function(e) {
-  discordr::create_discord_connection(
-    webhook = 'https://discord.com/api/webhooks/940930457070096444/qBSYJH0KETu992oDrdJBH20H1j4yPbBMZm2T3KNKZA5AU1LhRypZshQ0uKly9N_7jeGy',
-    username = "Weekly Expedition Update"
-  ) %>% 
-    discordr::send_webhook_message(
-      message = sprintf("Manual intervention required. (%s)", e$message), 
-      conn = .
-    )
+  
+  lorr::send_discord_message(
+    username = 'Weekly Expedition Update',
+    message = sprintf("Manual intervention required. (%s)", e$message)
+  )
+  
   print(e)
+  
 })
 
 rm(list = ls(all.names = TRUE))
+
 tryCatch({
+  
   tictoc::tic()
 
   #options(gargle_oauth_email = "Balco21@outlook.it")
@@ -62,13 +71,12 @@ tryCatch({
   
   tictoc::toc()
 }, error = function(e) {
-  discordr::create_discord_connection(
-    webhook = 'https://discord.com/api/webhooks/940930457070096444/qBSYJH0KETu992oDrdJBH20H1j4yPbBMZm2T3KNKZA5AU1LhRypZshQ0uKly9N_7jeGy',
-    username = "Weekly Google Spreadsheets Update"
-  ) %>% 
-    discordr::send_webhook_message(
-      message = sprintf("Manual intervention required. (%s)", e$message), 
-      conn = .
-    )
+  
+  lorr::send_discord_message(
+    username = 'Weekly Google Spreadsheets Update',
+    message = sprintf("Manual intervention required. (%s)", e$message)
+  )
+  
   print(e)
+  
 })
