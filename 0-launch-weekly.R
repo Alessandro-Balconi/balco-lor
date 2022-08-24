10a7b7b8d0996dccb51627d74a3b90119bf3e6dc
Alessandro Balconi <alessandro.balconi@outlook.it>
2022-08-24 12:17:58 +0000

    updated script to use "lorr" functions

--- 0-launch-weekly.R
+++ 0-launch-weekly.R
@@ -8,13 +8,9 @@ tryCatch({
   rm(list = ls(all.names = TRUE))
   source("/home/balco/dev/lor-meta-report/job-scripts/data-models/3-ranked_weekly_region_ngames.R")
 }, error = function(e) {
-  discordr::create_discord_connection(
-    webhook = 'https://discord.com/api/webhooks/940930457070096444/qBSYJH0KETu992oDrdJBH20H1j4yPbBMZm2T3KNKZA5AU1LhRypZshQ0uKly9N_7jeGy',
-    username = "Weekly database cleanup"
-  ) %>% 
-    discordr::send_webhook_message(
-      message = sprintf("Manual intervention required. (%s)", e$message), 
-      conn = .
-    )
+  lorr::send_discord_message(
+    username = "Weekly database cleanup",
+    message = sprintf("Manual intervention required. (%s)", e$message)
+  )
   print(e)
 })
