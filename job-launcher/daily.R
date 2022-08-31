a83ef08e2b7c29bf8c3a4490dd11da9dfd807b85
Alessandro Balconi <alessandro.balconi@outlook.it>
2022-08-31 10:38:59 +0000

    bugfix

--- job-launcher/daily.R
+++ job-launcher/daily.R
@@ -2,7 +2,7 @@
 cat(sprintf("--- %s --- \n", Sys.time()))
 
 # Send new matches from MongoDB to MySQL ---------------------------------------
-launch_scripts_in_folder(
+lorr::launch_scripts_in_folder(
   folder = "/home/balco/dev/lor-meta-report/job-scripts/daily-mongo-to-sql/",
   log_message = "MongoDB to MySQL: ",
   discord_username = "Daily MongoDB to MySQL Update",
@@ -10,7 +10,7 @@ launch_scripts_in_folder(
 )
 
 # Update data models -----------------------------------------------------------
-launch_scripts_in_folder(
+lorr::launch_scripts_in_folder(
   folder = "/home/balco/dev/lor-meta-report/job-scripts/daily-data-models/",
   log_message = "Data Models: ",
   discord_username = "Daily Data Models Update",
@@ -18,7 +18,7 @@ launch_scripts_in_folder(
 )
 
 # Send twitter posts -----------------------------------------------------------
-launch_scripts_in_folder(
+lorr::launch_scripts_in_folder(
   folder = "/home/balco/dev/lor-meta-report/job-scripts/daily-tweets/",
   log_message = "Twitter Posts: ",
   discord_username = "Daily Twitter Posts Update",
@@ -33,7 +33,7 @@ googlesheets4::gs4_auth_configure(api_key = google_creds$api_key)
 googlesheets4::gs4_auth(path = google_creds$auth_path)
 options(googlesheets4_quiet = TRUE)
 
-launch_scripts_in_folder(
+lorr::launch_scripts_in_folder(
   folder = "/home/balco/dev/lor-meta-report/job-scripts/daily-google-spreadsheets/",
   log_message = "Google Spreadsheets: ",
   discord_username = "Daily Google Spreadsheets Update",
