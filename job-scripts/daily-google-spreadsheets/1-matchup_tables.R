9abc04e28bbfb82f3494435df194d68a4e4ef1f6
Alessandro Balconi <alessandro.balconi@outlook.it>
2022-09-24 10:28:32 +0000

    added flixus petronus' spreadsheet to the updating list (with some ad hoc rules)

--- job-scripts/daily-google-spreadsheets/1-matchup_tables.R
+++ job-scripts/daily-google-spreadsheets/1-matchup_tables.R
@@ -27,13 +27,9 @@ update_spreadsheet <- function(n, time_frame, is_master, ss_id){
   # most played archetypes
   top <- tbl(con, "ranked_patch_matchups") %>% 
     filter(time_frame >= local(time_frame), is_master >= local(is_master)) %>% 
-    group_by(archetype_1) %>%
-    summarise(n = sum(n, na.rm = TRUE), .groups = "drop") %>% 
-    arrange(desc(n)) %>% 
+    count(archetype_1, wt = n, sort = TRUE) %>% 
     head(n = local(n)) %>% 
-    select(archetype_1) %>% 
-    collect() %>% 
-    pull()
+    pull(archetype_1)
   
   # same but wrapped (nicer looking in spreadsheets)
   top_wrap <- str_wrap(top, width = 8)
@@ -41,7 +37,10 @@ update_spreadsheet <- function(n, time_frame, is_master, ss_id){
   # collect matchups data
   x <- tbl(con, "ranked_patch_matchups") %>%
     filter(time_frame >= local(time_frame), is_master >= local(is_master)) %>% 
-    mutate(archetype_1 = if_else(archetype_1 %in% top, archetype_1, 'Other'), archetype_2 = if_else(archetype_2 %in% top, archetype_2, 'Other')) %>% 
+    mutate(
+      archetype_1 = if_else(archetype_1 %in% top, archetype_1, 'Other'), 
+      archetype_2 = if_else(archetype_2 %in% top, archetype_2, 'Other')
+    ) %>% 
     group_by(archetype_1, archetype_2) %>% 
     summarise(across(c(n, win), sum, na.rm = TRUE), .groups = 'drop') %>% 
     mutate(winrate = win / n) %>% 
@@ -59,12 +58,15 @@ update_spreadsheet <- function(n, time_frame, is_master, ss_id){
   total_n <- tbl(con, "ranked_patch_matchups") %>% 
     filter(time_frame >= local(time_frame), is_master >= local(is_master)) %>% 
     summarise(full_n = sum(n, na.rm = TRUE)) %>% 
-    collect() %>% 
     pull()
   
   # top decks info (playrate)
   y <- tbl(con, "ranked_patch_matchups") %>% 
-    filter(time_frame >= local(time_frame), is_master >= local(is_master), archetype_1 %in% top) %>%
+    filter(
+      time_frame >= local(time_frame), 
+      is_master >= local(is_master), 
+      archetype_1 %in% top
+    ) %>%
     group_by(archetype_1) %>% 
     summarise(games_played = sum(n, na.rm = TRUE), .groups = "drop") %>% 
     collect() %>% 
@@ -74,17 +76,40 @@ update_spreadsheet <- function(n, time_frame, is_master, ss_id){
   
   # top decks info (winrate)
   yy_wr <- tbl(con, "ranked_patch_matchups") %>% 
-    filter(time_frame >= local(time_frame), is_master >= local(is_master), archetype_1 %in% top) %>%
+    filter(
+      time_frame >= local(time_frame), 
+      is_master >= local(is_master), 
+      archetype_1 %in% top
+    ) %>%
     group_by(archetype_1) %>% 
     summarise(across(c(n, win), sum, na.rm = TRUE), .groups = 'drop') %>% 
     mutate(winrate = win / n) %>% 
     select(archetype_1, n, winrate) %>% 
     collect()
 
-  # prepare matchup table
+  # prepare matchup table ----
+  
+  # remove duplicate rows (mirror matches)
   x <- x %>% 
-    distinct() %>% 
-    mutate(across(c(winrate, n), function(x) ifelse(archetype_1 == archetype_2, NA, x))) %>% 
+    distinct() 
+  
+  # only for Flixus, keep number of mirror matches played
+  if(ss_id == "1Oe5-neLTksWlldS1lc1viiCwjKE_L3GbcQAC0T-l5N0"){
+    
+    x <- x %>% 
+      mutate(
+        winrate = ifelse(archetype_1 == archetype_2, 0.5, winrate),
+        n = ifelse(archetype_1 == archetype_2, n / 2, n)
+      )
+    
+  } else {
+    
+    x <- x %>% 
+      mutate(across(c(winrate, n), function(x) ifelse(archetype_1 == archetype_2, NA, x))) 
+    
+  }
+
+  x <- x %>% 
     mutate(across(archetype_2, str_wrap, width = 8)) %>% 
     mutate(across(archetype_1, factor, levels = c(top, 'Other'), ordered = TRUE)) %>% 
     mutate(across(archetype_2, factor, levels = c(top_wrap, 'Other'), ordered = TRUE)) %>% 
@@ -117,18 +142,28 @@ update_spreadsheet <- function(n, time_frame, is_master, ss_id){
     mutate(winrate = round(winrate*100, digits = 2)) %>% 
     rename(" " = archetype_1)
   
-  # update all sheets of the spreadsheet
-  range_write(data = x_wr,      ss = ss_id, sheet = "Winrate")
-  range_write(data = x_n,       ss = ss_id, sheet = "Number of Games")
-  range_write(data = deck_info, ss = ss_id, sheet = "Decks Information")
-  range_write(data = info,      ss = ss_id, sheet = "Data Information")
+  # update all sheets of the spreadsheet (this is for all but Flixus's SS)
+  if(ss_id != '1Oe5-neLTksWlldS1lc1viiCwjKE_L3GbcQAC0T-l5N0'){
+    
+    range_write(data = x_wr,      ss = ss_id, sheet = "Winrate")
+    range_write(data = x_n,       ss = ss_id, sheet = "Number of Games")
+    range_write(data = deck_info, ss = ss_id, sheet = "Decks Information")
+    range_write(data = info,      ss = ss_id, sheet = "Data Information")
+    
+    # names of the spreadsheet to update
+    ss_names <- sheet_names(ss_id)
+    
+    # adjust spacing of columns in the spreadsheet
+    walk(.x = ss_names, .f = ~range_autofit(ss = ss_id, sheet = .))
+    
+  }
   
-  # names of the spreadsheet to update
-  ss_names <- sheet_names(ss_id)
-  
-  # adjust spacing of columns in the spreadsheet
-  walk(.x = ss_names, .f = ~range_autofit(ss = ss_id, sheet = .))
-  
+  # for Flixus's SS do it a bit differently
+  if(ss_id == '1Oe5-neLTksWlldS1lc1viiCwjKE_L3GbcQAC0T-l5N0'){
+    range_write(data = x_wr,      ss = ss_id, sheet = "Matchup Table")
+    range_write(data = x_n,       ss = ss_id, sheet = "Number of Games")
+  }
+
 }
 
 # update all tables
@@ -139,5 +174,8 @@ update_spreadsheet(n = 40, time_frame = 1, is_master = 0, ss_id = "1K_V50pebtLj9
 update_spreadsheet(n = 40, time_frame = 2, is_master = 0, ss_id = "1BHK_ZsZadjD7WhqmmKXReBcWe_Rok9UkqE8y_ukTdmc")
 update_spreadsheet(n = 25, time_frame = 0, is_master = 1, ss_id = "1v2OtHSvmhYIvtUvCW7whvkuU1NIQa0teFyDtVxr-rnM")
 update_spreadsheet(n = 40, time_frame = 0, is_master = 1, ss_id = "1x2RhoT45Wdow6g8C3ZuatrxniqLrwjIOk4yn240G0GA")
+
+# this updates the spreadsheet of the Flixus Guy
+update_spreadsheet(n = 60, time_frame = 0, is_master = 0, ss_id = "1Oe5-neLTksWlldS1lc1viiCwjKE_L3GbcQAC0T-l5N0")
 
 DBI::dbDisconnect(con)
