b66cb45d44863ad3fc47504445ddca08e41433f9
Alessandro Balconi <alessandro.balconi@outlook.it>
2022-10-06 09:52:15 +0000

    fixed pandoc path (changed after Rstudio server update)

--- 0-launch-weekly-wednesday.R
+++ 0-launch-weekly-wednesday.R
@@ -2,20 +2,16 @@
 # This script runs every Wednesday (after the daily reports, around 18.30 CEST)
 tryCatch({
   cat(sprintf("--- %s --- \n", Sys.Date()))
-  Sys.setenv(RSTUDIO_PANDOC = "/usr/lib/rstudio-server/bin/pandoc")
+  Sys.setenv(RSTUDIO_PANDOC = "/usr/lib/rstudio-server/bin/quarto/bin/tools")
   tictoc::tic()
   source("/home/balco/dev/lor-meta-report/job-scripts/weekly-report/1-mysql-report.R")
   rm(list = ls(all.names = TRUE))
   source("/home/balco/dev/lor-meta-report/job-scripts/weekly-report/2-publish_report.R")
   tictoc::toc()
 }, error = function(e) {
-  discordr::create_discord_connection(
-    webhook = 'https://discord.com/api/webhooks/940930457070096444/qBSYJH0KETu992oDrdJBH20H1j4yPbBMZm2T3KNKZA5AU1LhRypZshQ0uKly9N_7jeGy',
-    username = "Weekly Report Release"
-  ) %>% 
-    discordr::send_webhook_message(
-      message = sprintf("Manual intervention required. (%s)", e$message), 
-      conn = .
-    )
+  lorr::send_discord_message(
+    username = 'Weekly Report Release',
+    message = sprintf("Manual intervention required. (%s)", e$message)
+  )
   print(e)
 })
