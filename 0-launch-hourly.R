25dd9c6cc9ae004436f01232365702fb006fcf09
Alessandro Balconi <alessandro.balconi@outlook.it>
2022-10-16 09:25:50 +0000

    folders structure cleanup

--- 0-launch-hourly.R
+++ 0-launch-hourly.R
@@ -1,14 +1,7 @@
-#Runs analysis + shows error if anything happens
-tryCatch({
-  source("/home/balco/dev/lor-meta-report/job-scripts/update-leaderboard/update_leaderboard.R")
-}, error = function(e) {
-  discordr::create_discord_connection(
-    webhook = 'https://discord.com/api/webhooks/940930457070096444/qBSYJH0KETu992oDrdJBH20H1j4yPbBMZm2T3KNKZA5AU1LhRypZshQ0uKly9N_7jeGy',
-    username = "Hourly leaderboard update"
-  ) %>% 
-    discordr::send_webhook_message(
-      message = "There was an error during the leaderboard update. Manual intervention required.", 
-      conn = .
-    )
-  print(e)
-})
+# Update Leadeboard Tables ---------------------------------------------------
+lorr::launch_scripts_in_folder(
+  folder = "/home/balco/dev/lor-meta-report/job-scripts/hourly-leaderboard-update/",
+  log_time = FALSE,
+  discord_username = "Hourly Leaderboard Update",
+  discord_message = "Manual intervention required. "
+)
