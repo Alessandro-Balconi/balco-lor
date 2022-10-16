73faabccb2265de40a7a8902f0f95f88529107fb
Alessandro Balconi <alessandro.balconi@outlook.it>
2022-10-16 10:07:15 +0000

    added reminder for future me

--- other-scripts/seasonal-open-rounds/open_rounds_main_script.R
+++ other-scripts/seasonal-open-rounds/open_rounds_main_script.R
@@ -2,9 +2,10 @@
 # 1. update the parameters right below these comments [ss_id, seasonal_day, cutoff_day]
 # 2. GIVE TO THE AUTOUPDATER EDIT PERMISSIONS ON THE SPREADSHEET !
 # 3. THE DAY OF THE SEASONAL (BEFORE 11 AM): start a local job that runs the trycatch wrapper job
-# 4. (OPTIONAL) remove rows from the db at the end of the seasonal (SEE CODE BELOW)
+# 4. SLOW DOWN API CALLS IN THE LAUNCH_EUROPE.R SCRIPT (optional but do it: 0.1 -> 0.2 and restart)
+# 5. REMOVE ROWS FROM FB AT THE END OF SEASONAL (SEE CODE BELOW)
 
-# PER IL PUNTO 4 CONTROLLARE CHE QUESTA TABELLA SIA VUOTA
+# PER IL PUNTO 5 CONTROLLARE CHE QUESTA TABELLA SIA VUOTA
 #DBI::dbExecute(conn = con, statement = "DELETE FROM seasonal_match_data;")
 # E CHE QUESTO FILE NON ESISTA: already_collected.rds
 
