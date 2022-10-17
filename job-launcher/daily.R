d9a3ae48f20b15e1586d07442b59c76c8e0ab176
Alessandro Balconi <alessandro.balconi@outlook.it>
2022-10-17 19:18:16 +0000

    remove some logs

--- job-launcher/daily.R
+++ job-launcher/daily.R
@@ -29,7 +29,7 @@ lorr::launch_scripts_in_folder(
 # set google API Key & Oauth credentials
 options(googlesheets4_quiet = TRUE)
 google_creds <- config::get("google", file = "/home/balco/my_rconfig.yml")
-gargle::oauth_app_from_json(google_creds$client_secret)
+invisible(gargle::oauth_app_from_json(google_creds$client_secret))
 googlesheets4::gs4_auth_configure(api_key = google_creds$api_key)
 googlesheets4::gs4_auth(path = google_creds$auth_path)
 
