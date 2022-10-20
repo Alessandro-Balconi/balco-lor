a2e52e14dc80bf6f5e400a085d5ba31d610eca4e
Alessandro Balconi <alessandro.balconi@outlook.it>
2022-10-20 14:02:53 +0000

    removed "utils_ranked_patch_decklists_cards" table update

--- queries/job-scripts/daily-data-models/tmp_ranked_patch_decklists_cards.sql
+++ queries/job-scripts/daily-data-models/tmp_ranked_patch_decklists_cards.sql
@@ -1,4 +1,48 @@
 CREATE TABLE tmp_ranked_patch_decklists_cards AS (
+  WITH 
+  decklists AS (
+    SELECT DISTINCT
+      COALESCE(aa.new_name, i.archetype) AS archetype,
+      i.deck_code,
+      i.cards
+    FROM 
+      ranked_match_metadata_30d md
+    JOIN
+      ranked_match_info_30d i
+    USING(match_id)
+    LEFT JOIN
+      utils_archetype_aggregation aa
+    ON
+      i.archetype = aa.old_name
+    WHERE
+      md.game_start_time_utc >= '{patch_release_date}'
+  ),
+  data_cards AS (
+    SELECT
+      d.archetype,
+      d.deck_code,
+      SUBSTRING_INDEX(SUBSTRING_INDEX(d.cards, ' ', un.n), ' ', -1) AS cards
+    from
+      utils_numbers un
+    inner join 
+      decklists d
+    on 
+      CHAR_LENGTH(d.cards) - CHAR_LENGTH(REPLACE(d.cards, ' ', '')) >= un.n - 1
+    ORDER BY
+      archetype,
+      deck_code,
+      n
+  ),
+  utils_ranked_patch_decklists_cards AS (
+    SELECT 
+      archetype,
+      deck_code,
+      SUBSTRING_INDEX(cards, ':', 1) AS `count`,
+      SUBSTRING_INDEX(cards, ':', -1) AS card_code
+    FROM
+      data_cards
+  )
+    
   SELECT 
     rpd.archetype, 
     region, 
